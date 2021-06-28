2867a0575da0fad78100709e30e3ae0d91a456d7
Nic Annau <nannau@uvic.ca>
2021-06-28 16:51:43 -0700

    Adds EOF tests

--- DoWnGAN/tests/test_losses.py
+++ DoWnGAN/tests/test_losses.py
@@ -7,6 +7,13 @@ from DoWnGAN.losses import (
 import pytest
 import torch
 import numpy as np
+import xarray as xr
+
+from DoWnGAN.dataloader import xr_standardize_field
+from sklearn.decomposition import PCA
+from pkg_resources import resource_filename
+from sklearn.metrics import mean_squared_error
+
 
 # Shape is:
 # batch_size, channels, x, y
@@ -35,6 +42,14 @@ device = torch.device("cpu")
 @pytest.fixture
 def example_data():
     return {"content": [hr, fake, device], "grads": [hrg, fakeg, device]}
+
+@pytest.fixture
+def actual_data():
+    dataroot = resource_filename("DoWnGAN", "data/interim_2000-10-01_to_2013-09-30.nc")
+    real = xr.open_dataset(dataroot)
+    X = real["u10"][:100, ...]
+    X = np.array(xr_standardize_field(X))
+    return {"data": X}
 
 
 def test_content_loss(example_data):
@@ -99,3 +114,44 @@ def test_vorticity_loss(example_data):
 
     assert isinstance(vloss, float)
     assert np.isclose(vloss, vloss_truth, atol=0.0001)
+
+
+def test_eof_loss(actual_data):
+
+    n_comps = 20
+
+    # Load data
+    X = actual_data["data"]
+
+    # Get EOFs
+    pca = PCA(n_comps)
+    pca.fit(X.reshape(X.shape[0], X.shape[1] * X.shape[2]))
+
+    # Normalize by magnitudes of eigenvalues
+    Z = (
+        pca.components_.reshape(n_comps, X.shape[1] * X.shape[2])
+        / pca.explained_variance_[:, np.newaxis]
+    )
+
+    # Convert to tensors
+    X = torch.from_numpy(X)
+    Z = torch.from_numpy(Z)
+
+    # Reshape to project
+    X = torch.reshape(X, (X.shape[0], X.shape[1] * X.shape[2]))
+    Z = Z.unsqueeze(-1)
+
+    # Corrupt a signal to test against magnitudes
+    X_c = torch.clone(X)
+    X_c[0, ...] = X[0, ...] + np.random.normal(scale=5, size=(X.shape[1]))
+
+    magnitude = torch.matmul(X, Z)
+    magnitude_c = torch.matmul(X_c, Z)
+
+    assert X_c.size() == X.size()
+    # Test corrupted signal is non-zero difference over all
+    # EOFs
+    assert not np.isclose(
+        mean_squared_error(magnitude[:, :, 0], magnitude_c[:, :, 0]), 0.0, atol=1e-6
+    )
+    assert np.isclose(mean_squared_error(magnitude[:, :, 0], magnitude[:, :, 0]), 0.0)
