4839bfdb31b8ba440e648bb714ef06a513aff609
Nic Annau <nannau@uvic.ca>
2022-01-10 20:45:34 -0800

    Removes content loss

--- DoWnGAN/hyperparams.py
+++ DoWnGAN/hyperparams.py
@@ -6,6 +6,8 @@ from DoWnGAN.losses import (
     wass_loss
 )
 import torch
+import torch.nn as nn
+
 import os
 
 device = torch.device("cuda:0")
@@ -15,7 +17,7 @@ gp_lambda = 10
 critic_iterations = 5
 batch_size = 64
 gamma = 0.01
-content_lambda = 5
+content_lambda = 0.0 #5
 ncomp = 75
 lr = 0.00025
 
@@ -28,7 +30,10 @@ device = device
 
 # Frequency separation parameters
 freq_sep = False
-padding = 2 # Valid only if freq_sep is True
+filter_size = 5
+padding = filter_size // 2
+low = nn.AvgPool2d(filter_size, stride=1, padding=0)
+rf = nn.ReplicationPad2d(padding)
 
 # Whether to load preprocessed data
 already_preprocessed = True
